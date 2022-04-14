104a2a4313b511862cec15471901d7b010f2e8c2
Security OverFlow (0xSOF) <78023331+SecurityOverFlow@users.noreply.github.com>
2022-04-14 16:46:25 +0200

    Add files via upload

---
+++ BuFFer0verFl0w.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//#include <conio.h>
+#include <string.h>
+//this script created By Abdalrahman(0xSOF)
+int main(void){
+  char buffer[15];
+  int A0xSOF=0;
+   printf("Enter Your String: ");
+    gets(buffer);
+      if(strcmp(buffer,"0xSOF")){
+         printf("Successful");
+  }else{
+      printf("FALED");
+      A0xSOF=1;
+
+    } 
+if(A0xSOF){
+  printf("You Are ROOT");
+
+ }
+return 0;
+}
