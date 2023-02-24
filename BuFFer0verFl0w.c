fb68f8cd6364607745b0e94668921ef02c1d7dd6
Abdalrahman(0x80) <78023331+abdalrahman0x80@users.noreply.github.com>
2023-02-24 13:07:30 +0200

    Update BuFFer0verFl0w.c

--- BuFFer0verFl0w.c
+++ BuFFer0verFl0w.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-/*
-if your OS is Windows Append To this Header 
-<conio.h> Header File
-*/
-int main(void){
+/* if your OS is Windows Append To this Header <conio.h> Header File */
+int main(){
   char buffer[15];
   int i = 0;
-   printf("Enter Your String: ");
-    gets(buffer);
-      if(strcmp(buffer,"0x80")){
-         printf("Successful");
+  printf("Enter Your String: ");
+  gets(buffer);
+    if(strcmp(buffer,"admin")){
+     printf("Successfull");
   }else{
       printf("FALED");
       i=1;
-
-    } 
-if(i){
-  printf("You Are ROOT");
-
+  } 
+ if(i){
+   printf("You Are ROOT");
  }
 return 0;
 }
